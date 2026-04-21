17524c28fe1d165269def684bb63b19bb321f005
victoriapares <mavictoriapares@alumnos.cei.es>
2026-04-21 20:54:33 +0200

    Actualización 2  del proyecto final

--- js/index.js
+++ js/index.js
@@ -76,6 +76,7 @@ window.addEventListener("scroll", () => {
 window.addEventListener("DOMContentLoaded", () => {
   let header = document.querySelector(".header");
   let headerSmall = document.querySelector(".header_small");
+  
 
   let images = document.querySelectorAll(".div_img");
 
@@ -151,24 +152,43 @@ window.addEventListener("scroll", () => {
 
 
 /*--------GALERÍA FOTOS--------*/
-      // Array de imágenes para la galería
+/*--------GALERÍA FOTOS--------*/
+
+/* Las primeras visibles en HTML son 01 y 02 */
+/* El array empieza desde la siguiente foto */
+
 let imagenesIzquierda = [
-  "Media/grid_1_color.jpg",
-  "Media/grid_2_color.jpg",
-  "Media/grid_3_color.jpg",
-  "Media/grid_4_color.jpg",
-  "Media/grid_5_color.jpg",
+  { src: "Media/grid_3_color.jpg", numero: "03" },
+  { src: "Media/grid_5_color.jpg", numero: "05" },
+  { src: "Media/grid_7_color.jpg", numero: "07" },
+  { src: "Media/grid_9_color.jpg", numero: "09" },
+  { src: "Media/grid_11_color.jpg", numero: "11" },
+  { src: "Media/grid_13_color.jpg", numero: "13" },
+  { src: "Media/grid_15_color.jpg", numero: "15" },
+  { src: "Media/grid_17_color.jpg", numero: "17" },
+  { src: "Media/grid_19_color.jpg", numero: "19" },
+  { src: "Media/grid_21_color.jpg", numero: "21" },
+  { src: "Media/grid_23_color.jpg", numero: "23" },
+  { src: "Media/grid_25_color.jpg", numero: "25" },
+  { src: "Media/grid_27_color.jpg", numero: "27" },
+  { src: "Media/grid_29_color.jpg", numero: "29" }
 ];
 
 let imagenesDerecha = [
-  "Media/grid_6_color.jpg",
-  "Media/grid_7_color.jpg",
-  "Media/grid_8_color.jpg",
-  "Media/grid_9_color.jpg",
-  "Media/grid_10_color.jpg",
-  "Media/grid_11_color.jpg",
-  "Media/grid_12_color.jpg",
-  "Media/grid_13_color.jpg",
+  { src: "Media/grid_4_color.jpg", numero: "04" },
+  { src: "Media/grid_6_color.jpg", numero: "06" },
+  { src: "Media/grid_8_color.jpg", numero: "08" },
+  { src: "Media/grid_10_color.jpg", numero: "10" },
+  { src: "Media/grid_12_color.jpg", numero: "12" },
+  { src: "Media/grid_14_color.jpg", numero: "14" },
+  { src: "Media/grid_16_color.jpg", numero: "16" },
+  { src: "Media/grid_18_color.jpg", numero: "18" },
+  { src: "Media/grid_20_color.jpg", numero: "20" },
+  { src: "Media/grid_22_color.jpg", numero: "22" },
+  { src: "Media/grid_24_color.jpg", numero: "24" },
+  { src: "Media/grid_26_color.jpg", numero: "26" },
+  { src: "Media/grid_28_color.jpg", numero: "28" },
+  { src: "Media/grid_30_color.jpg", numero: "30" }
 ];
 
 let index1 = 0;
@@ -177,53 +197,65 @@ let index2 = 0;
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
 
+let caption1 = document.querySelector(".div_gallery__izquierda .caption_gallery");
+let caption2 = document.querySelector(".div_gallery__derecha .caption_gallery");
+
+function cambiarImagen(img, nuevoSrc, callback) {
+  img.style.transition = "opacity 0.2s ease";
+  img.style.opacity = 0;
+
+  setTimeout(() => {
+    img.src = nuevoSrc;
+
+    img.onload = () => {
+      callback(img);
+      img.style.opacity = 1;
+    };
+  }, 300);
+}
+
+function ajustarTamaño(img) {
+  if (img.naturalWidth > img.naturalHeight) {
+    img.classList.add("horizontal");
+    img.classList.remove("vertical");
+  } else {
+    img.classList.add("vertical");
+    img.classList.remove("horizontal");
+  }
+}
+
+/* Mantiene 01 y 02 iniciales del HTML */
+caption1.textContent = "( 01 )";
+caption2.textContent = "( 02 )";
+
+/* Click izquierda */
 img1.addEventListener("click", () => {
+  cambiarImagen(
+    img1,
+    imagenesIzquierda[index1].src,
+    ajustarTamaño
+  );
+
+  caption1.textContent = `( ${imagenesIzquierda[index1].numero} )`;
+
   index1 = (index1 + 1) % imagenesIzquierda.length;
-  img1.src = imagenesIzquierda[index1];
 });
 
+/* Click derecha */
 img2.addEventListener("click", () => {
+  cambiarImagen(
+    img2,
+    imagenesDerecha[index2].src,
+    ajustarTamaño
+  );
+
+  caption2.textContent = `( ${imagenesDerecha[index2].numero} )`;
+
   index2 = (index2 + 1) % imagenesDerecha.length;
-  img2.src = imagenesDerecha[index2];
 });
-    // Array de imágenes para la galería
 
+/*--------GALERÍA FOTOS--------*/
 
-
-
-    // Diferentes tamaños de las imágenes
-let imgGaleriaIzquierda = document.getElementById("img1");
-
-function tamañoGaleriaIzquierda() {
-
-    if (imgGaleriaIzquierda.width > imgGaleriaIzquierda.height) {
-        imgGaleriaIzquierda.classList.add("horizontal");
-        imgGaleriaIzquierda.classList.remove("vertical");
-    } else {
-        imgGaleriaIzquierda.classList.add("vertical");
-        imgGaleriaIzquierda.classList.remove("horizontal");
-    }
-}
-
-imgGaleriaIzquierda.onload = tamañoGaleriaIzquierda;
-
-
-
-let imgGaleriaDerecha = document.getElementById("img2");
-
-function tamañoGaleriaDerecha() {
-
-    if (imgGaleriaDerecha.width > imgGaleriaDerecha.height) {
-        imgGaleriaDerecha.classList.add("horizontal");
-        imgGaleriaDerecha.classList.remove("vertical");
-    } else {
-        imgGaleriaDerecha.classList.add("vertical");
-        imgGaleriaDerecha.classList.remove("horizontal");
-    }
-}
-
-imgGaleriaDerecha.onload = tamañoGaleriaDerecha;
-    // Diferentes tamaños de las imágenes
 
 /*--------GALERÍA FOTOS--------*/
 
